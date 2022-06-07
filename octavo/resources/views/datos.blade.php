34d7ef46fb70d95376a72e28f00332c6831d4970
AnaCaren <prinssana002@gmail.com>
2022-06-07 16:09:28 -0500

    Proyecto Octavo

---
+++ octavo/resources/views/datos.blade.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+
+<head>
+
+
+    <meta charset="UTF-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <title>Página de contacto</title>
+
+
+</head>
+
+
+<body>
+
+
+    <div clas="container">
+
+
+        <div class="row">
+
+
+            <div class="col">
+
+
+               <h2>Enviar mensaje</h2>
+
+
+              
+<form >
+
+
+                    <div class="form-group">
+
+
+                       <input type="text"
+name="nombre" class="form-control" placeholder="Escribe tu nombre" >
+
+
+                    </div>
+
+
+                    <div class="form-group">
+
+
+                       <input type="email" name="email" class="form-control"placeholder="Escribe
+tu email">
+
+
+                    </div>
+
+
+                    <div class="form-group">
+
+
+                       <textarea name="mensaje" class="form-control"placeholder="Escribe tus comentarios" ></textarea>
+
+
+                    </div>
+
+
+                    <div class="form-group">
+
+
+                       <input type="submit" value="Enviar"  class="btn btn-primary">
+
+
+                    </div>
+
+
+               </form>
+
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+</body>
+
+
+</html>
+
+
+ 
